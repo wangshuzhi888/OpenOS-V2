e0a6732ff0631e613690ee4f92053d4af8458819
wangshuzhi888 <97008350+wangshuzhi888@users.noreply.github.com>
2023-05-23 19:01:04 +0800

    Update wget+.lua

--- bin/wget+.lua
+++ bin/wget+.lua
@@ -33,3 +33,46 @@ local code,text,header=handle.response()
 code=math.floor(code,1)
 assert(code==200,"Server returned HTTP response code: "..tostring(code).." "..(text or "OK").." for URL: "..args[1])
 print("code: "..tostring(code).." "..(text or "OK"))
+if header["Content-Length"]==nil then
+  Length=math.huge
+else
+  Length=tonumber(header["Content-Length"][1])
+end
+if header["Server"]==nil then
+  Server="Unknow"
+else
+  Server=header["Server"][1]
+end
+print("Length: "..Length.." ("..math.floor(Length/1024,1).." KB)")
+print("Server: "..Server)
+filename=string.split(args[1],"/")
+filename=filename[#filename]
+if args[2] then
+  filename=args[2]
+end
+local path=os.getenv("PWD")
+if path=="/" then
+    filename="/"..filename
+else
+    filename=path.."/"..filename
+end
+file=fs.open(filename,"w")
+bytes=0
+repeat
+  data=handle.read(math.huge)
+  computer.pullSignal(0)
+  if data then
+    bytes=bytes+#data
+    file:write(data)
+    a=math.floor(bytes/Length*100,1)
+    if resX>117 then
+      io.write("\rdownloading "..tostring(a).."%"..string.rep(" ",3-#tostring(a)).." ["..string.rep("█",a)..string.rep(" ",100-a).."]")
+    else
+      b=math.floor(a/5,1)
+      io.write("\rdownloading "..tostring(a).."%"..string.rep(" ",3-#tostring(a)).." ["..string.rep("█",b)..string.rep(" ",20-b).."]")
+    end
+  end
+until not data
+handle.close()
+file:close()
+print("\nDone.")
